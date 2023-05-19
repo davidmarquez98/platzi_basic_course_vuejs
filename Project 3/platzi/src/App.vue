135ad78bf72ae1202aa719d6b00655630a445cad
David Marquez <david.Marquez@outlook.com.ar>
2023-05-18 22:52:00 -0300

    wip course

---
+++ Project 3/platzi/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="container">
+    <Header />
+  </div>
+</template>
+
+<style scoped>
+  .container{
+    width:100%;
+    height:100%;;
+  }
+</style>
---
+++ Project 3/platzi/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">
+    <nav>
+      <div>
+        <span>App Exchange</span>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+</style>
